21f8f1488070d9b3c59dfd292bdaca2c62d4a842
Vidit Shah <vidit0210@outlook.com>
2020-09-03 22:47:48 +0530

    l0

---
+++ EducativeDataStructures/List/List101.py
@@ -0,0 +1,11 @@
+new_list = ['a', 'apple', 23, 3.14]
+
+# Create a list of elements
+example_list = [3.145, 'apple', 23]
+# empty_list
+empty_list = []
+# Sequence_list
+sequence_list = list(range(10))
+print(example_list)
+print(empty_list)
+print(sequence_list)
