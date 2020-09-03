e3972a45789a6d3e3704c4187af78701484f1f85
Vidit Shah <vidit0210@outlook.com>
2020-09-03 23:27:06 +0530

    del

--- EducativeDataStructures/List/List104.py
+++ EducativeDataStructures/List/List104.py
@@ -9,3 +9,19 @@ print(l[:])
 l = list(range(10))
 print(l[0::2])
 print(l[-3::-2])
+
+
+# Modifying Value using the exisitng Value..
+x = list(range(10))
+print(x)
+x[1:4] = [99, 98, 97]
+print(x)
+
+# deleting elements from the list.
+d = list(range(100))
+print(d)
+del d[0]
+print(d)
+# Delete Every Alternate element from the list
+del d[::2]
+print(d)
