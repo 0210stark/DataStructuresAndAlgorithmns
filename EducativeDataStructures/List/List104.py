500da8cdbb15d818c607b124a3c78301a2e2735e
Vidit Shah <vidit0210@outlook.com>
2020-09-03 23:22:05 +0530

    Step Index

--- EducativeDataStructures/List/List104.py
+++ EducativeDataStructures/List/List104.py
@@ -1,2 +1,11 @@
 # Slicing inde randg [)
 l = list(range(10))
+print(l)
+print(l[0:2])
+print(l[2:])
+print(l[:])
+
+# Step Index [start,stop,index]
+l = list(range(10))
+print(l[0::2])
+print(l[-3::-2])
